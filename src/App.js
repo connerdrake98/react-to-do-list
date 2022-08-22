42d8b61fbd9d9854a48037c0cb8eb6d38cd1dd59
Conner Drake <conner@boardlovers.com>
2022-08-21 20:47:26 -0400

    Pushing First Version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  const TITLE = "To-Do List";
+
+  const [inputFieldContents, setInputFieldContents] = React.useState("");
+
+  const [toDoList, setToDoList] = React.useState([]);
+
+  const handleInput = (event) => {
+    setInputFieldContents(event.target.value);
+  };
+
+  const handleAddItem = function () {
+    setToDoList([...toDoList, inputFieldContents]);
+  };
+
+  const handleDeleteItem = function (description) {
+    setToDoList(toDoList.filter((item) => item !== description));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <h1 class="centered">{TITLE}</h1>
+      <LabeledForm
+        label="Enter Item: "
+        buttonText="Add to List"
+        position="centered"
+        inputFieldContents={inputFieldContents}
+        handleInput={handleInput}
+        addItem={handleAddItem}
+      />
+      <ToDoList
+        position="flex-left"
+        bordered={true}
+        items={toDoList}
+        onRemoveItem={handleDeleteItem}
+      />
+    </>
+  );
+}
+
+function LabeledForm({
+  label,
+  buttonText,
+  position,
+  inputFieldContents,
+  handleInput,
+  addItem,
+}) {
+  return (
+    <span class={position}>
+      <p class={position}>{label}</p>
+      <Form
+        class={position}
+        position={position}
+        isFocused
+        inputFieldContents={inputFieldContents}
+        buttonText={buttonText}
+        handleInput={handleInput}
+        addItem={addItem}
+      />
+    </span>
+  );
+}
+
+function Form({
+  position,
+  isFocused,
+  inputFieldContents,
+  buttonText,
+  handleInput,
+  addItem,
+}) {
+  const inputRef = React.useRef();
+  isFocused = false;
+  isFocused = true;
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label class={position}></label>
+      <input onChange={handleInput} ref={inputRef}></input>
+      <Button
+        inputFieldContents={inputFieldContents}
+        buttonText={buttonText}
+        addItem={addItem}
+      />
+    </>
+  );
+}
+
+function Button({ inputFieldContents, buttonText, addItem }) {
+  return <button onClick={addItem}>{buttonText}</button>;
+}
+
+function ToDoList({ position, items, onRemoveItem, bordered }) {
+  return (
+    <ul>
+      {items.map((item) => (
+        <Item
+          description={item}
+          onRemoveItem={onRemoveItem}
+          position={position}
+          bordered={bordered}
+        />
+      ))}
+    </ul>
+  );
+}
+
+function Item({ description, onRemoveItem, position, bordered }) {
+  const handleRemoveItem = () => onRemoveItem(description);
+
+  return (
+    <div class={bordered ? `${position} bordered` : position}>
+      <p>{description}</p>
+      <button type="button" onClick={handleRemoveItem}>
+        resolve
+      </button>
     </div>
   );
 }
